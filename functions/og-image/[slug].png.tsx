936faa2bc3635f2e72c2560a674a0bba7d2c4355
Yukiya Nakagawa <yukiya@monicle.co.jp>
2024-06-12 22:16:48 +0900

    feat: colored

--- functions/og-image/[slug].png.tsx
+++ functions/og-image/[slug].png.tsx
@@ -2,10 +2,24 @@ import { ImageResponse } from "@cloudflare/pages-plugin-vercel-og/api";
 
 export const onRequest: PagesFunction<Env> = async ({ params }) => {
   const slug = params.slug;
-  console.log('onRequest', 'slug', slug);
+  console.log("onRequest", "slug", slug);
 
   return new ImageResponse(
-    <div style={{ display: "flex" }}>Hello, {slug}!</div>,
+    (
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          height: "100%",
+          backgroundColor: "#00CED1",
+          alignItems: "center",
+          justifyContent: "center",
+          color: 'white',
+        }}
+      >
+        Hello, {slug}!
+      </div>
+    ),
     {
       width: 1200,
       height: 630,
